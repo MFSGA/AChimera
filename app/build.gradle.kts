499360633fd9e0cd9281788132cfaa969b875989
MFSGA <chimera_rust@proton.me>
2026-08-15 20:06:31 +0800

    chore(release): bump version to v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.ktlint)
 }
 
-val baseVersionName = "0.6.1"
+val baseVersionName = "0.7.0"
 val Project.verName: String
     get() {
         val commit = exec("git rev-parse --short HEAD")
